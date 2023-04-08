3d9110e042964dfe14d86f186c752b89cc3f3c1c
Kane John <joh21423@d.umn.edu>
2023-04-08 14:04:20 -0500

    [Lab Work] - Building a Graph Data Structure

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,45 @@
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 
 public class Main {
-    public static void main(String[] args, HashMap<Object, Object> adjacencyList) {
+    public static void Main(String[] args, Vertex[] vertices) {
         System.out.println("Hello world!");
 
         Graph g = new Graph();
-        Vertex v0 = new Vertex( "v0" );
-        Vertex v1 = new Vertex( "v1" );
-        Vertex v2 = new Vertex( "v2" );
-        Vertex v3 = new Vertex( "v3" );
-        Vertex v4 = new Vertex( "v4" );
-        Vertex v5 = new Vertex( "v5" );
+        Vertex v0 = new Vertex("v0");
+        Vertex v1 = new Vertex("v1");
+        Vertex v2 = new Vertex("v2");
+        Vertex v3 = new Vertex("v3");
+        Vertex v4 = new Vertex("v4");
+        Vertex v5 = new Vertex("v5");
         g.addVertex(v0);
         g.addVertex(v1);
         g.addVertex(v2);
         g.addVertex(v3);
         g.addVertex(v4);
-        g.addVertex(v5);  // version `
-        // this is version 1 with the hashmap
-        // Vertex v0 = new Vertex( "v0" );
-        // g.addVertex(v0);
-        // Vertex v1 = new Vertex("v1");
-        // g.addVertex(v1);
-     //   g.addVertex(v0);
-        //g.addVertex();
-
-        // version 2
-      //  g.addEdge(0, 1) ;
+        g.addVertex(v5);
         g.addEdge(v0, v1);
         g.addEdge(v0, v2);
-        g.addEdge(v1, v3);
+        g.addEdge(v1, v2);
+        g.addEdge(v1, v1);
         g.addEdge(v2, v3);
-        g.addEdge(v3, v4);
-        g.addEdge(v4, v5);
-        // g.addEdge(v0, v1);
-       // g.addEdge(0, 2);
+        g.addEdge(v2, v4);
+        g.addEdge(v2, v1);
+        g.addEdge(v2, v5);
+        g.addEdge(v2, v0);
+        g.addEdge(v4, v0);
+        g.addEdge(v5, v1);
+        g.addEdge(v5, v3);
+        g.addEdge(v3, v5);
 
-       // g.printGraph();
-
-        Map<Vertex, List<Vertex>> adjacencyList = g.getAdjacencyList();
-        for (Map.Entry<Vertex, List<Vertex>> entry : adjacencyList.entrySet()) {
-            System.out.print(entry.getKey() + " -> ");
-            System.out.println(entry.getValue());
-
-        System.out.println("Total number of edges in the graph: " + edgeSum);
+        for (Vertex v : vertices) {
+            System.out.print(v + " -> ");
+            LinkedList<Vertex> neighbors = g.getNeighbors(v);
+            for (Vertex n : neighbors) {
+                System.out.print(n + ", ");
+            }
+            System.out.println();
+        }
+        System.out.println("Total number of edges in the graph: " + g.getNumEdges());
     }
-} // refacotr to hashmap and
+}
+// refacotr to hashmap and
